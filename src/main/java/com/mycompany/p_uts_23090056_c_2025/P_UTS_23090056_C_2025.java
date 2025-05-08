c9b85f9ab338e54f47e9c3bcb4ebf8ccd15f112b
Ariftri3 <ariftri815@gmail.com>
2025-05-08 09:22:04 +0700

    first commit

---
+++ src/main/java/com/mycompany/p_uts_23090056_c_2025/P_UTS_23090056_C_2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p_uts_23090056_c_2025;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class P_UTS_23090056_C_2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
